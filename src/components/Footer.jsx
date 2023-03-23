c79893931e706acb8826bbf01a5eefcb99385fbc
witekZWitkowic <witoldadamus94@gmail.com>
2023-03-23 10:47:30 +0100

    Updated icons in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { linkedin,facebook,instagram } from '../assets'
 const Footer = () => {
   return (
-    <section className='w-full flex flex-col mt-5'>
-      <div className='w-[90%] h-[1px] bg-dimWhite m-auto'/>
+    <section className='w-full flex flex-col pt-10'>
+      <div className='w-[90%] h-[1px] bg-zinc-800 m-auto'/>
       <div className='flex justify-between my-7'>
         <div>
           <h2 className='text-gradient'>&#174; Web-Masters.pl </h2>
         </div>
         <div className='flex'>
-          <img src={instagram}/>
-          <img src={linkedin}/>
+          <img src={instagram} className='mr-4'/>
+          <img src={linkedin} className='mr-4'/>
           <img src={facebook}/>
         </div>
       </div>
