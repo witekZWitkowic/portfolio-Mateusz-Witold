9c3410aab1f1830063122e406ad817b18780dab4
witekZWitkowic <witoldadamus94@gmail.com>
2023-03-27 10:22:55 +0200

    Form Changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import Button from './Button'
+import Textarea from './Textarea'
+import Input from './Input'
 import styles from '../style'
-import { Input } from "@vechaiui/react"
 
 
 const Contact = () => {
   return (
-    <section className={`flex flex-col justify-between ${styles.boxWidth} my-12`}>
-      <div>
-
+    <section class="">
+      <div class="py-8 lg:py-16 px-4 mx-auto max-w-[700px]">
+        <h2 class="mb-4 text-[48px] font-bold text-center text-gray-900 dark:text-white">Kontakt</h2>
+        <p class="mb-8 lg:mb-10 text-center text-dimWhite font-light text-[15px]">
+          Masz jakieś pytania? Z chęcią na nie odpowiemy!
+        </p>
+        <form action="#" class="space-y-4">
+            <Input type="email" forName="email" placeholderName="Napisz swój email." labelName="E-mail"/>
+            <Input type="text" forName="text" placeholderName="Napisz temat wiadomości." labelName="Temat"/>
+            <Textarea forName="message" labelName="Treść wiadomości" placeholderName="Napisz treść swojej wiadomości."/>
+            <div className='w-[150px] mx-auto'>
+              <Button name="Wyślij!"/>
+            </div>
+        </form>
       </div>
-      <div>
-
-      </div>
-      <Button />
     </section>
   )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = ({type, forName, labelName, placeholderName}) => {
+  return (
+    <div className='flex flex-col mx-auto'>
+        <label for={forName} class="block mb-1 text-[18px] font-medium text-white">{labelName}</label>
+        <input type={type} id={forName} class="shadow-sm bg-black-gradient border border-teal-300 text-gray-900 outline-none text-sm rounded-lg focus:border-teal-600 block p-2.5 " placeholder={placeholderName} required />
+    </div>
+  )
+}
+
+export default Input
