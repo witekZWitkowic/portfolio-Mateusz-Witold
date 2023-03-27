5fb709548f2c29b877ec04cf42af2349164a677b
witekZWitkowic <witoldadamus94@gmail.com>
2023-03-27 10:28:51 +0200

    TExt-White in contact

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({type, forName, labelName, placeholderName}) => {
   return (
     <div className='flex flex-col mx-auto'>
         <label for={forName} class="block mb-1 text-[18px] font-medium text-white">{labelName}</label>
-        <input type={type} id={forName} class="shadow-sm bg-black-gradient border border-teal-300 text-gray-900 outline-none text-sm rounded-lg focus:border-teal-600 block p-2.5 " placeholder={placeholderName} required />
+        <input type={type} id={forName} class="shadow-sm bg-black-gradient border border-teal-300 text-white outline-none text-sm rounded-lg focus:border-teal-600 block p-2.5 " placeholder={placeholderName} required />
     </div>
   )
 }
