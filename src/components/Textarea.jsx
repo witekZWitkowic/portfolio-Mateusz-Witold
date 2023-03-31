93812228db36d45457944184df1340112c7f0e99
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-03-31 13:03:32 +0200

    Textarea fix

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Textarea = ({forName, labelName, placeholderName}) => {
+const Textarea = ({forName, labelName, placeholderName, name}) => {
   return (
     <div className="sm:col-span-2">
         <label htmlFor={forName} className="block mb-2 text-[20px] font-medium text-white">{labelName} <span className='text-red-600'>*</span></label>
-        <textarea id={forName} rows="6" className="ow-sm bg-black-gradient border-[2px] border-teal-700 text-white text-sm rounded-lg valid:bg-black-gradient focus:outline-none focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5"  placeholder={placeholderName}>
+        <textarea id={forName} name={name} rows="6" className="ow-sm bg-black-gradient border-[2px] border-teal-700 text-white text-sm rounded-lg valid:bg-black-gradient focus:outline-none focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5"  placeholder={placeholderName}>
         </textarea>
     </div>
   )
